4537ac6a7c77a37ffcf9f47e217ae95129f845f7
emchudd <eclhuddleston@gmail.com>
2021-03-09 09:12:50 -0600

    Created html file and js file

---
+++ task.js
@@ -0,0 +1,26 @@
+// Displays Tasks
+let display
+// Adds Task
+let add
+// Removes Tasks
+let remove 
+// Closes task Manager
+let close 
+// For main screen
+let taskMan = true;
+prompt(`TASK MANAGER 
+
+What would you like to do? (Enter one of the options below)
+"TASKS" - Display All Tasks
+"NEW" - Add A New Task
+"REMOVE" - Remove A Task
+"CLOSE" - Close the Task Manager`
+
+//how do i put a space here?
+);
+
+
+
+
+
+
